b763952beccaa361b90455c1f10fff75119341cf
Chloe Mingay <cmingay@fastmail.fm>
2020-01-01 17:04:35 +0000

    Game component holds state and passes click function as prop

--- QuizGame/ClientApp/src/App.js
+++ QuizGame/ClientApp/src/App.js
@@ -9,26 +9,11 @@ import './custom.css'
 export default class App extends Component {
     static displayName = App.name;
 
-    constructor(props) {
-        super(props);
-        this.state = { questions: [], loading: true };
-    }
-
-    componentDidMount() {
-        this.populateQuestionsData();
-    }
-
   render () {
     return (
       <Layout>
-            <Route path='/game' render={(props) => <Game {...props} questions={this.state.questions} />} />
+            <Route path='/game' component={Game} />
       </Layout>
     );
-    }
-
-    async populateQuestionsData() {
-        const response = await fetch('api/questions/1');
-        const data = await response.json();
-        this.setState({ questions: data, loading: false });
     }
 }
--- QuizGame/ClientApp/src/components/Game.js
+++ QuizGame/ClientApp/src/components/Game.js
@@ -2,16 +2,46 @@ import React, { Component } from 'react';
 import { Questions } from './Questions';
 
 export class Game extends Component {
-  static displayName = Game.name;
+    static displayName = Game.name;
 
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = { questions: [], loading: true };
+        this.s = 1;
+        console.log(this.s);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount() {
+        this.populateQuestionsData(this.s);
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : <Questions questions={this.state.questions} />
+
       return (
         <div>
         <div>  Lets Play the Game  </div>
-              <Questions questions={this.props.questions} />
+              {contents}
+              <div>
+                  <button onClick={this.handleClick} type="button">Click Me!</button>
+              </div>
         </div>
+
     );
     }
 
+    async handleClick() {
+        console.log(this)
+        this.s = this.s + 1;
+        this.populateQuestionsData(this.s);
+    }
 
+    async populateQuestionsData(roundNumber) {
+        const response = await fetch('api/questions/' + roundNumber);
+        const data = await response.json();
+        this.setState({ questions: data, loading: false });
+    }
 }
